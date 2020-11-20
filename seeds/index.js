8f9908e1f2e03167ef98366a8d1cf98c2a5919e2
Brian Tan <briantan37@gmail.com>
2020-11-20 13:34:54 -0800

    Implemented bootstrap into the html pages

--- seeds/index.js
+++ seeds/index.js
@@ -1,34 +1,39 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campground');
-const cities = require('./cities');
-const {descriptors , places} = require('./seedHelper');
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelper");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('Database connection established');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+	console.log("Database connection established");
 });
 
 const randPicker = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${randPicker(descriptors)} ${randPicker(places)}`
-        });
-        await camp.save();
-    }
-}
+	await Campground.deleteMany({});
+	for (let i = 0; i < 50; i++) {
+		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 100) + 11;
+		const camp = new Campground({
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			title: `${randPicker(descriptors)} ${randPicker(places)}`,
+			image: "https://source.unsplash.com/collection/182053/",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum sodales nisi, et aliquet sapien varius ac. Fusce gravida nec ex in pretium. Cras aliquet tristique metus non ultricies. Aenean eget mauris vitae diam iaculis gravida ac id mauris. Cras hendrerit vestibulum nisl, in aliquam augue facilisis vitae. Praesent finibus tortor quis velit sollicitudin, sed tristique eros bibendum. Sed pharetra viverra lacus, semper bibendum lacus ornare ac.",
+			price: price,
+		});
+		await camp.save();
+	}
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
+	mongoose.connection.close();
 });

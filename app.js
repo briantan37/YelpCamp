e343aee178730f10e7eab670a0d1d14fb1df9d01
Brian Tan <briantan37@gmail.com>
2020-12-01 15:54:24 -0800

    Installed session and setup inital config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -32,9 +33,21 @@ app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
+
+const sessionConfig = {
+    secret: 'Zd2tGB!16QweAAA32=u6pBAzcBdIGM7NZPy^^+-*jz^OLIB7c',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+};
+
+app.use(session(sessionConfig));
 
 app.get('/', (req, res) => {
     res.render('home');

10c3323a767bca94ece746b1a57c495c176cb6aa
Brian Tan <briantan37@gmail.com>
2020-12-01 16:01:37 -0800

    Implemented flash for adding new campground with middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -26,6 +27,7 @@ db.once('open', function () {
 const app = express();
 
 app.engine('ejs', ejsMate);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -33,6 +35,7 @@ app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
 
@@ -48,6 +51,13 @@ const sessionConfig = {
 };
 
 app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.render('home');

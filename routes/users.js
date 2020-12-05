87cf30bbd5e0dfa75e705533cbe5782b697fcba7
Brian Tan <briantan37@gmail.com>
2020-12-04 22:21:55 -0800

    Implemented login route and page

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
@@ -12,7 +13,6 @@ router.post('/register', catchAsync(async (req, res) => {
         const {email, username, password} = req.body;
         const user = new User({email, username});
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
         req.flash('success','Welcome to Yelp Camp!');
         res.redirect('/campgrounds');
     }
@@ -22,4 +22,12 @@ router.post('/register', catchAsync(async (req, res) => {
     }
 }));
 
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds');
+})
 module.exports = router;

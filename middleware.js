16221583e813f61544783b03e9ce17a5f5c8f9e4
Brian Tan <briantan37@gmail.com>
2020-12-06 16:49:31 -0800

    Implement authorization checks for review

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const ExpressError = require('./utils/ExpressError');
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
@@ -30,6 +31,17 @@ module.exports.isAuthor = async(req, res, next) => {
     }
     next();
 }
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review  = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
 
 module.exports.validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/review');
 const Campground = require('../models/campground');
-const { validateReview, isLoggedIn } = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res, next) => {
         const camp = await Campground.findById(req.params.id);
@@ -17,7 +17,7 @@ router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res, next) =
     })
 );
 
-router.delete('/:reviewId', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
         const { id, reviewId } = req.params;
         await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
         await Review.findByIdAndDelete(reviewId);

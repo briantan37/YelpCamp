754aac0a5312d0f42416b0d8c2ea4bc32c4848c5
Brian Tan <briantan37@gmail.com>
2020-12-10 09:18:23 -0800

    Added cluster map to the index page

--- app.js
+++ app.js
@@ -86,6 +86,6 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('Server live on port 3000');
+app.listen(3001, () => {
+    console.log('Server live on port 3001');
 });

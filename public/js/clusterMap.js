efdb8da5f0ba124745db8349db7b848801bb2920
Brian Tan <briantan37@gmail.com>
2020-12-10 12:12:47 -0800

    Added zoom controls on map

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -3,13 +3,13 @@
 // https://account.mapbox.com
 mapboxgl.accessToken = mapToken;
 var map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
 });
 
-console.log(campgrounds);
+map.addControl(new mapboxgl.NavigationControl({showCompass:false}));
 
 map.on('load', function () {
     // Add a new source from our GeoJSON data and

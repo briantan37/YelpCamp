5222525f316bc11e17326b2ddabeaf15a2dcd30f
Brian Tan <briantan37@gmail.com>
2020-12-15 17:46:53 -0800

    Changed ports to be production or development

--- app.js
+++ app.js
@@ -95,6 +95,8 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('Server live on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server live on port ${port}`);
 });
